9aa6f66eb87d779b786d14d149cd4314c530a37d
RobertoFORTs <bobfortesneto@gmail.com>
2024-04-06 14:30:22 -0400

    initial layout

---
+++ app/src/main/java/com/example/mobile_essay1/ui/adapters/SongsAdapter.java
@@ -0,0 +1,59 @@
+package com.example.mobile_essay1.ui.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.mobile_essay1.R;
+
+import java.util.List;
+
+public class SongsAdapter extends BaseAdapter {
+    private Context mContext;
+    private List<String> mSongNames;
+
+    public SongsAdapter(Context context, List<String> songNames) {
+        mContext = context;
+        mSongNames = songNames;
+    }
+
+    @Override
+    public int getCount() {
+        return mSongNames.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mSongNames.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.song_layout, null);
+        }
+
+        TextView textViewSongName = view.findViewById(R.id.text_view_song_name);
+        Button buttonPlay = view.findViewById(R.id.button_play);
+
+        textViewSongName.setText(mSongNames.get(position));
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/mobile_essay1/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/mobile_essay1/ui/dashboard/DashboardFragment.java
@@ -4,28 +4,38 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.mobile_essay1.R;
 import com.example.mobile_essay1.databinding.FragmentDashboardBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.example.mobile_essay1.ui.adapters.*;
 
 public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
+    private GridView mGridView;
+    private SongsAdapter mSongsAdapter;
+    private List<String> mSongNames;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
-
+        DashboardViewModel dashboardViewModel = new ViewModelProvider(this).get(DashboardViewModel.class);
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        mSongNames = new ArrayList<>();
+        mGridView = root.findViewById(R.id.grid_view_songs);
+        mSongsAdapter = new SongsAdapter(requireContext(), mSongNames);
+        mGridView.setAdapter(mSongsAdapter);
+
         return root;
     }
 
