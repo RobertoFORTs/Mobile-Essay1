a0fa4ee7d7afcec4619aaa71cd4c721e797ca3bb
Daniel Schutz <109694403+Daniel-Schutz@users.noreply.github.com>
2024-04-09 20:31:09 -0400

    adding musics and photos

---
+++ app/src/main/java/com/example/mobile_essay1/ItemListView.java
@@ -0,0 +1,22 @@
+package com.example.mobile_essay1;
+
+public class ItemListView {
+    private int icone;
+    private String texto;
+    public ItemListView(int icone, String texto){
+        this.icone=icone;
+        this.texto=texto;
+    }
+    public int getIcone() {
+        return icone;
+    }
+    public void setIcone(int icone) {
+        this.icone = icone;
+    }
+    public String getTexto() {
+        return texto;
+    }
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+}
